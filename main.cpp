acf6861bbdf6c2d16a6480ee5537108dafe4b72b
Abe <abenanduri1@gmail.com>
2024-09-28 21:06:43 -0700

    Second commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+//COMSC-210 | Lab 14 | Abhishek Nanduri
+// IDE used: VS code
+
 #include<iostream>
 
 using namespace std; 
@@ -5,15 +8,20 @@ using namespace std;
 class Color {
     
     public:
-       
-        int red; 
-        int green; 
-        int blue; 
+       //private variables only accessible by the class
+        int Red; 
+        int Green; 
+        int Blue; 
 
     private: 
+        // getters and setters
+        void setRed(int r) { Red = r; }
+        void setGreen(int g) { Green = g; }
+        void setBlue(int b) { Blue = b; }
+        int getRed()   { return Red; }
+        int getGreen() { return Green; }
+        int getBlue()  { return Blue; }
 
-        
-
-
+       
 
 };
