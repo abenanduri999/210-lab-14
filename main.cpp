b75f2a63c7bcf105e13c25be8abe4664b2622059
Abe <abenanduri1@gmail.com>
2024-09-28 20:54:40 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std; 
+
+class Color {
+    
+    public:
+       
+        int red; 
+        int green; 
+        int blue; 
+
+    private: 
+
+        
+
+
+
+};
